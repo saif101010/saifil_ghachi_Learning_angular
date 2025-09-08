577fe02c5ef9dd2c0b5cad09e2e478ae58f95a26
w0857343 <w0857343@myscc.ca>
2025-09-08 14:23:40 -0400

    added variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('untitled');
+
+  //lets make a variable
+  name: String = "saif"
+
+  //once you assign a var type, it is forever that type
+
 }
